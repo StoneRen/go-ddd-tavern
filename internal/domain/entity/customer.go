fda9d16bd54ba9e7c370b3612d40c683d1f9c4dd
StoneRen <ren@stone.com>
2023-03-29 15:30:09 +0800

    feat(customer): 增加创建工厂和测试

--- internal/domain/entity/customer.go
+++ internal/domain/entity/customer.go
@@ -1,5 +1,14 @@
 package entity
 
+import (
+	"errors"
+	"github.com/google/uuid"
+)
+
+var (
+	ErrInvalidPerson = errors.New("invalid person")
+)
+
 // Customer 聚合
 // 可变的为指针类型
 // 不可变的为值类型
@@ -7,4 +16,21 @@ type Customer struct {
 	person       *Person
 	products     []*Item
 	transactions []Transaction
+}
+
+func NewCustomer(name string) (Customer, error) {
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	person := &Person{
+		Name: name,
+		Id:   uuid.New(),
+	}
+
+	return Customer{
+		person:       person,
+		products:     make([]*Item, 0),
+		transactions: make([]Transaction, 0),
+	}, nil
 }
